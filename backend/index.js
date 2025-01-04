17980540cfcedf7cd06efce8d7c79a070b87e7ab
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2025-01-04 17:13:13 +0530

    adding prima amd schemas..

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,46 @@
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware }from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import { buildContext } from "graphql-passport";
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+import prisma from "./lib/prisma.js"
+const app = express();
+dotenv.config();
 
-import mergedResolvers from "./resolvers/index.js"
-import mergedTypeDefs from "./typeDefs/index.js"
 
+
+const httpServer=http.createServer(app)
 
 const server= new ApolloServer({
     typeDefs: mergedTypeDefs,
     resolvers:mergedResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
-const { url }=await startStandaloneServer(server);
+await server.start()
 
+app.use("/graphql",cors({
+    origin:"*",
+    credentials: true,
+}),
+  express.json(),
+  expressMiddleware(server,{
+    context: async({req,res})=> buildContext({req,res})
+  })
+);
 
-console.log("server running on :",url);
+   await new Promise((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve),
+  );
+
+  await prisma.$connect();
+  console.log("✅ Prisma connected successfully");
+
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 
 
